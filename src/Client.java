4b29209b3c86d4a2d32be2c97628331942fee2a1
michaelthomson0797 <michaelthomson0797@gmail.com>
2017-03-20 18:43:00 -0400

    updated client

--- src/Client.java
+++ src/Client.java
@@ -1,56 +1,26 @@
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Client {
-    
 	public static Hospital[] hospitals;
-	static Scanner in;
+	public static ArrayList<ArrayList<String>> countyList;
+	public static String[] stateList = {"ALL", "AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA",
+						  "GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA","MD",
+						  "ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH",
+						  "NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI",
+						  "SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"};
+
 	
-    public static void main(String[] args) throws FileNotFoundException {
-    	in = new Scanner(System.in);
-        hospitals = CSVReader.init();
-        mainMenu();
-        
-    }
-    
-    public static void mainMenu() {
-    	boolean done = false;
-    	while(!done) {
-    		System.out.println("1. Search Hospitals"
-    			           + "\n2. Sort Hospitals"
-    			           + "\n3. Quit");
-    		
-    		System.out.print("enter numeric option: ");
-    		int input = in.nextInt();
-    		
-    		switch (input) {
-			case 1:
-				searchMenu();
-				break;
-			case 2:
-				sortMenu();
-				break;
-			case 3:
-				done = true;
-				break;
-			default:
-				System.out.println("Invalid input, please enter a number 1-3 inclusive");
+	public static void main(String[] args) throws Exception {
+		CSVReader.init();
+		GUI frame = new GUI();
+		frame.setVisible(true);
+	}
+	
+	public static String printHospitals(Hospital[] a) {
+		String output = "";
+	    	for (int i = 0; i < a.length; i++) {
+	    		output += a[i].toString();
 			}
-    	}
-    	
-    }
-    
-    public static void sortMenu() {
-    	//needs to be implemented
-    }
-    
-    public static void searchMenu() {
-    	//needs to be implemented
-    }
-    
-    public static void printHospitals(Hospital[] a) {
-    	for (int i = 0; i < 100; i++) {
-			System.out.println(a[i].toString());
-		}
-    }
+	    return output;
+	 }
 }
