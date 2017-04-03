6fa16e6f9caa360c1f09bac540521fa90b52e82c
Amit binu <Amit binu>
2017-04-03 11:42:58 -0400

    Modified Client.java, so it makes the appropriate graph

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Client {
+	
 	public static Hospital[] hospitals;
 	public static ArrayList<ArrayList<String>> countyList;
 	public static String[] stateList = {"ALL", "AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA",
@@ -12,6 +14,21 @@ public class Client {
 	
 	public static void main(String[] args) throws Exception {
 		CSVReader.init();
+		
+		Scanner input = new Scanner(System.in); // CHANGE THIS SO IT TAKES THE RIGHT HOSPITAL / HOSPITAL ID from GUI.
+		System.out.println("Enter a hospital id: ");  // CHANGE THIS SO IT TAKES THE RIGHT HOSPITAL / HOSPITAL ID from GUI.
+		int hospital_id = input.nextInt(); // CHANGE THIS SO IT TAKES THE RIGHT HOSPITAL / HOSPITAL ID from GUI.
+		
+		Graph_input inputForGraph = new Graph_input(hospitals, hospital_id);
+
+		//The index values of hospitals was used to make the graph.
+		Graph g = new Graph(hospitals, inputForGraph.input, inputForGraph.distan);
+		
+		
+/*		for (Edge i : g.adj(0)){
+			System.out.println(i.toString());
+		} */  
+		
 		GUI frame = new GUI();
 		frame.setVisible(true);
 	}
